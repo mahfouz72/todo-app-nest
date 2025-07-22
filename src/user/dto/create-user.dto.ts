b172f41b078cedf07bafc1886dd680dc240257a5
Mohamed Mahfouz <mohamadmahfouz39@gmail.com>
2025-07-22 13:20:56 +0300

    Use pipes for validation and parse int transformation (#5)

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDTO {
+  @IsString()
+  @IsNotEmpty()
   username: string;
+
+  @IsString()
+  @MinLength(8)
   password: string;
 }
