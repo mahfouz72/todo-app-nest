fb9e4be2f551365d7e284d788d0dd3a9781ebaff
mahfouz72 <mohamadmahfouz39@gmail.com>
2025-07-29 18:06:30 +0300

    Create Todo modal for post todo

--- frontend/src/components/TodoList.tsx
+++ frontend/src/components/TodoList.tsx
@@ -1,4 +1,6 @@
 import type {Todo} from "../types/todo.ts";
+import CreateTodo from "./CreateTodo.tsx";
+import {useState} from "react";
 
 type TodoListProps = {
     todos: Todo[],
@@ -7,26 +9,35 @@ type TodoListProps = {
 }
 
 export default function TodoList({todos, selectedTodo, onSelect}: TodoListProps) {
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => {
+        setModal(!modal);
+    };
+
     return (
-        <div
-            className="max-w-md mt-15 p-4 rounded-xl bg-white shadow-md h-128 flex flex-col justify-between">
-            <ul className="pl-5 space-y-3 overflow-y-auto">
-                {todos.map((todo) => (
-                    <li key={todo.id} className="flex flex-row gap-2">
-                        <input type="checkbox" className="cursor-pointer"/>
-                        <div
-                            className={`cursor-pointer p-2 rounded flex-1 ${
-                                selectedTodo?.id === todo.id ? 'bg-blue-100' : 'hover:bg-gray-100'}`}
-                            onClick={() => onSelect(todo)}
-                        >
-                            {todo.title}
-                        </div>
-                    </li>
-                ))}
-            </ul>
-            <button className="py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
-                Add Todo
-            </button>
-        </div>
+        <>
+            <div className="max-w-md mt-15 p-4 rounded-xl bg-white shadow-md h-128 flex flex-col justify-between">
+                <ul className="pl-5 space-y-3 overflow-y-auto">
+                    {todos.map((todo) => (
+                        <li key={todo.id} className="flex flex-row gap-2">
+                            <input type="checkbox" className="cursor-pointer"/>
+                            <div
+                                className={`cursor-pointer p-2 rounded flex-1 ${
+                                    selectedTodo?.id === todo.id ? 'bg-blue-100' : 'hover:bg-gray-100'}`}
+                                onClick={() => onSelect(todo)}
+                            >
+                                {todo.title}
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+                <button className="py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+                        onClick={() => setModal(true)}>
+                    Add Todo
+                </button>
+            </div>
+            <CreateTodo toggle={toggle} modal={modal} />
+        </>
     )
 }
