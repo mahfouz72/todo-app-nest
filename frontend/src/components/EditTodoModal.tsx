3ad07c0e9aeac28941cb0b5c6217c4dbc21facd9
mahfouz72 <mohamadmahfouz39@gmail.com>
2025-07-30 19:11:17 +0300

    Implement edit todo functionality

---
+++ frontend/src/components/EditTodoModal.tsx
@@ -0,0 +1,156 @@
+import type {Todo} from "../types/todo.ts";
+import {z} from "zod";
+import {type SubmitHandler, useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useEffect} from "react";
+import {Button, Modal, ModalBody, ModalFooter, ModalHeader} from "reactstrap";
+import {api} from "../api/axios.ts";
+
+type createTodoProps = {
+    todo: Todo | null | undefined,
+    modal: boolean,
+    toggle: () => void,
+    onUpdate: (TodoId: number, todo: Todo) => void,
+}
+
+const schema = z.object({
+    title: z.string().nonempty({message: "Title is required"}),
+    description: z.string(),
+    status: z.enum(['PENDING', 'IN_PROGRESS', 'COMPLETED']),
+    priority: z.enum(['LOW', 'MEDIUM', 'HIGH']),
+    dueDate: z.date(),
+})
+
+type formFields = z.infer<typeof schema>;
+
+export default function EditTodoModal({todo, modal, toggle, onUpdate}: createTodoProps) {
+    const {register, handleSubmit, reset, formState: {errors}} = useForm<formFields>({
+        resolver: zodResolver(schema),
+        defaultValues: {
+            title: todo?.title,
+            description: todo?.description,
+            status: todo?.status,
+            priority: todo?.priority,
+            dueDate: todo?.dueDate ? new Date(todo.dueDate) : new Date(),
+        }
+    });
+
+    useEffect(() => {
+        import('bootstrap/dist/css/bootstrap.min.css');
+    }, []);
+
+    useEffect(() => {
+        reset({
+            title: todo?.title,
+            description: todo?.description,
+            status: todo?.status,
+            priority: todo?.priority,
+            dueDate: todo?.dueDate ? new Date(todo.dueDate) : new Date(),
+        });
+    }, [todo, reset]);
+
+    const onSubmitHandler: SubmitHandler<formFields> = async (data) => {
+        try {
+            const token = localStorage.getItem("token");
+            const response = await api.put(`/todos/${todo?.id}`, data, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            console.log(response.data);
+            const todoResponse: Todo = response.data;
+            onUpdate(todoResponse.id, todoResponse);
+            toggle();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return (
+        <div>
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader toggle={toggle}>Create Todo</ModalHeader>
+                <ModalBody>
+                    <form onSubmit={handleSubmit(onSubmitHandler)}>
+                        {/* title */}
+                        <div>
+                            <label className="form-label">Title</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                {...register("title")}
+                            />
+                            {errors.title && (
+                                <div className="text-danger text-sm">{errors.title.message}</div>
+                            )}
+                        </div>
+
+
+                        {/* description */}
+                        <div>
+                            <label className="form-label">Description</label>
+                            <textarea
+                                className="form-control"
+                                {...register("description")}
+                            />
+                            {errors.description && (
+                                <div className="text-danger text-sm">{errors.description.message}</div>
+                            )}
+                        </div>
+
+                        {/* Status */}
+                        <div>
+                            <label className="form-label">Status</label>
+                            <select className="form-control" {...register("status")}>
+                                <option value="PENDING">PENDING</option>
+                                <option value="IN_PROGRESS">IN_PROGRESS</option>
+                                <option value="COMPLETED">COMPLETED</option>
+                            </select>
+                            {errors.status && (
+                                <div className="text-danger text-sm">{errors.status.message}</div>
+                            )}
+                        </div>
+
+                        {/* Priority */}
+                        <div>
+                            <label className="form-label">Priority</label>
+                            <select className="form-control"  {...register("priority")}>
+                                <option value="LOW">LOW</option>
+                                <option value="MEDIUM">MEDIUM</option>
+                                <option value="HIGH">HIGH</option>
+                            </select>
+                            {errors.priority && (
+                                <div className="text-danger text-sm">{errors.priority.message}</div>
+                            )}
+                        </div>
+
+                        {/* Due date */}
+                        <div>
+                            <label className="form-label">Due Date</label>
+                            <input
+                                type="datetime-local"
+                                className="form-control"
+                                {...register("dueDate", {
+                                    valueAsDate: true,
+                                })}
+                            />
+                            {errors.dueDate && (
+                                <div className="text-danger text-sm">{errors.dueDate.message}</div>
+                            )}
+                        </div>
+
+                    </form>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={handleSubmit(onSubmitHandler)}>
+                        Update
+                    </Button>{' '}
+                    <Button color="secondary" onClick={toggle}>
+
+                        Cancel
+                    </Button>
+                </ModalFooter>
+            </Modal>
+        </div>
+    )
+}
--- frontend/src/components/TodoDetails.tsx
+++ frontend/src/components/TodoDetails.tsx
@@ -1,20 +1,29 @@
 import type {Todo} from "../types/todo.ts";
+import {FaEdit} from "react-icons/fa";
+import {useState} from "react";
+import EditTodoModal from "./EditTodoModal.tsx";
 
 type TodoDetailsProps = {
     todo: Todo | null | undefined,
+    onUpdate: (id: number, todo: Todo) => void,
 }
 
-export default function TodoDetails({todo}: TodoDetailsProps) {
+export default function TodoDetails({todo, onUpdate}: TodoDetailsProps) {
+
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => {
+        setModal(!modal);
+    };
+
     function getStatusColor(status: "PENDING" | "IN_PROGRESS" | "COMPLETED" | undefined) {
         if (status === "PENDING") {
             return "text-yellow-600"
         } else if (status === "IN_PROGRESS") {
             return "text-blue-600";
-        }
-        else if (status === "COMPLETED") {
+        } else if (status === "COMPLETED") {
             return "text-green-600"
-        }
-        else {
+        } else {
             return "text-gray-600";
         }
     }
@@ -24,47 +33,59 @@ export default function TodoDetails({todo}: TodoDetailsProps) {
             return "text-green-600"
         } else if (priority === "MEDIUM") {
             return "text-blue-600";
-        }
-        else if (priority === "HIGH") {
+        } else if (priority === "HIGH") {
             return "text-yellow-600"
-        }
-        else {
+        } else {
             return "text-red-600";
         }
     }
 
     return (
-        <div className="max-w-md h-128 mt-15 p-4 bg-white rounded-xl shadow-md space-y-4">
-            <div>
-                <p className="text-sm text-gray-500">Title</p>
-                <p className="text-lg text-gray-900">{todo?.title}</p>
-            </div>
+        <>
+            <div className="max-w-md h-128 mt-15 p-4 bg-white rounded-xl shadow-md space-y-4">
+                <div className="flex items-center justify-between">
+                    <div>
+                        <p className="text-sm text-gray-500">Title</p>
+                        <p className="text-lg text-gray-900">{todo?.title}</p>
+                    </div>
+                    <div>
+                        <button
+                            onClick={() => setModal(true)}
+                            className="text-blue-500 hover:text-blue-700 text-lg flex-11/12"
+                            title="Edit Todo"
+                        >
+                            <FaEdit/>
+                        </button>
+                    </div>
+                </div>
 
-            <div>
-                <p className="text-sm text-gray-500">Description</p>
-                <p className="text-base text-gray-800">
-                    {todo?.description}
-                </p>
-            </div>
-
-            <div className="flex items-center justify-between">
                 <div>
-                    <p className="text-sm text-gray-500">Status</p>
-                    <p className={`text-base font-medium ${getStatusColor(todo?.status)}`}>
-                        {todo?.status}
+                    <p className="text-sm text-gray-500">Description</p>
+                    <p className="text-base text-gray-800">
+                        {todo?.description}
                     </p>
                 </div>
+
+                <div className="flex items-center justify-between">
+                    <div>
+                        <p className="text-sm text-gray-500">Status</p>
+                        <p className={`text-base font-medium ${getStatusColor(todo?.status)}`}>
+                            {todo?.status}
+                        </p>
+                    </div>
+                    <div>
+                        <p className="text-sm text-gray-500">Priority</p>
+                        <p className={`text-base font-medium  ${getPriorityColor(todo?.priority)}`}>
+                            {todo?.priority}
+                        </p>
+                    </div>
+                </div>
                 <div>
-                    <p className="text-sm text-gray-500">Priority</p>
-                    <p className={`text-base font-medium  ${getPriorityColor(todo?.priority)}`}>
-                        {todo?.priority}
-                    </p>
+                    <p className="text-sm text-gray-500">Due Date</p>
+                    <p className="text-base text-gray-800">{todo?.dueDate}</p>
                 </div>
             </div>
-            <div>
-                <p className="text-sm text-gray-500">Due Date</p>
-                <p className="text-base text-gray-800">{todo?.dueDate}</p>
-            </div>
-        </div>
+            <EditTodoModal todo={todo} modal={modal} toggle={toggle} onUpdate={onUpdate}/>
+        </>
     );
 }
