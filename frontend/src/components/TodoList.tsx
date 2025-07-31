cd9fbea4d4576275a914ccb3f76b51c7ea27936c
Mohamed Mahfouz <mohamadmahfouz39@gmail.com>
2025-07-31 19:41:11 +0300

    Implement mark as completed functionality (#22)

--- frontend/src/components/TodoList.tsx
+++ frontend/src/components/TodoList.tsx
@@ -10,9 +10,10 @@ type TodoListProps = {
     onSelect: (todo: Todo) => void,
     onCreate: (todo: Todo) => void,
     onDelete: (id: number) => void,
+    onUpdate: (id: number, updatedTodo: Todo) => void,
 }
 
-export default function TodoList({todos, selectedTodo, onSelect, onCreate, onDelete}: TodoListProps) {
+export default function TodoList({todos, selectedTodo, onSelect, onCreate, onDelete, onUpdate}: TodoListProps) {
     const [modal, setModal] = useState(false);
 
     const toggle = () => {
@@ -34,16 +35,40 @@ export default function TodoList({todos, selectedTodo, onSelect, onCreate, onDel
         }
     }
 
+    const handleCompleted= async (todo: Todo) => {
+        try {
+            const updatedTodo = {
+                ...todo,
+                status: (todo.status === 'COMPLETED') ? 'IN_PROGRESS' : 'COMPLETED'
+            };
+            const token = localStorage.getItem("token");
+            const response = await api.put(`/todos/${updatedTodo?.id}`, updatedTodo, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            const todoResponse: Todo = response.data;
+            onUpdate(todoResponse.id, todoResponse);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <>
             <div className="max-w-md mt-15 p-4 rounded-xl bg-white shadow-md h-128 flex flex-col justify-between">
                 <ul className="pl-5 space-y-3 overflow-y-auto">
                     {todos.map((todo) => (
                         <li key={todo.id} className="flex flex-row gap-2">
-                            <input type="checkbox" className="cursor-pointer"/>
+                            <input type="checkbox" className="cursor-pointer"
+                                   onChange={() => handleCompleted(todo)}
+                                   checked={todo.status === 'COMPLETED'}
+                            />
                             <div
-                                className={`cursor-pointer p-2 rounded flex-1 ${
-                                    selectedTodo?.id === todo.id ? 'bg-blue-100' : 'hover:bg-gray-100'}`}
+                                className={`cursor-pointer p-2 rounded flex-1
+                                        ${selectedTodo?.id === todo.id ? 'bg-blue-100' : 'hover:bg-gray-100'}
+                                        ${todo.status === 'COMPLETED' ? 'line-through text-gray-400' : ''}
+                                `}
                                 onClick={() => onSelect(todo)}
                             >
                                 {todo.title}
